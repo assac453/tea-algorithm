5ba3459d96d23d936720d73d339948a065765406
assac453 <ritterfrosch@yandex.ru>
2024-02-28 10:27:25 +0300

    init

---
+++ src/test/java/com/assac453/tea/TeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assac453.tea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
